f4466c153f5ace356c2d60c7311798dc4aa63cef
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-12-13 11:39:27 +0000

    first commit

---
+++ dest/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Primitive Types
+// number, bool and string
+let empName = 'Test';
+let employeeName;
+employeeName = 'Testing';
+let isPermanent = true;
+let age = 30;
+
+
+let multiply = (num) => {
+    return 10 * num
+}
+
+console.log(multiply('ten'))
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Primitive Types
+// number, bool and string
+
+let empName = 'Test'
+
+let employeeName:string;
+
+employeeName = 'Testing'
+
+let isPermanent:boolean = true
+
+let age = 30
+
+let multiply = (num:number) => {
+    return 10 * num
+}
+
+console.log(multiply(10))
